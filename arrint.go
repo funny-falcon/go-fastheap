0191f43e620fb8dc8dd59abedbf05d9e0c8e499b
Sokolov Yura aka funny_falcon <funny.falcon@gmail.com>
2013-10-08 18:51:26 +0400

    4arr arrint

--- arrint.go
+++ arrint.go
@@ -5,10 +5,11 @@ import "fmt"
 var _ = fmt.Errorf
 
 type intArr struct {
-	ref   IntValue
-	value int64
-	up    uint32
-	down  [3]uint32
+	ref        IntValue
+	value      int64
+	up         uint32
+	upi, downl uint16
+	down       [4]uint32
 }
 
 type ArrInt struct {
@@ -18,6 +19,24 @@ type ArrInt struct {
 	top  uint32
 	len  uint32
 	free uint32
+}
+
+func (h *ArrInt) String() string {
+	res := fmt.Sprintf("{s: %d t: %d l: %d f: %d [", h.size, h.top, h.len, h.free)
+	for i, a := range h.heap {
+		for j, ar := range *a {
+			if ar.ref != nil {
+				res += fmt.Sprintf("%d={v: %d u: %d ui: %d dl: %d [%d %d %d %d]} ", i*pageSize+j, ar.value, ar.up, ar.upi, ar.downl, ar.down[0], ar.down[1], ar.down[2], ar.down[3])
+				for k, kk := range ar.down {
+					if kk > 0 && h.at(kk).value < ar.value {
+						panic(fmt.Errorf("child value less %d %d %d %+v %+v", i*pageSize+j, kk, k, ar, h.at(kk)))
+					}
+				}
+			}
+		}
+	}
+	res += "]}"
+	return res
 }
 
 func (h *ArrInt) Size() int {
@@ -84,49 +103,56 @@ func (h *ArrInt) delete(at uint32) {
 	h.free = at
 	h.size--
 	var downi uint32
-	if t.down[0] != 0 {
-		if t.down[1] == 0 {
-			downi = t.down[0]
-		} else if t.down[2] == 0 {
-			if h.at(t.down[1]).value < h.at(t.down[0]).value {
-				h.putUnder(t.down[1], t.down[0])
-				downi = t.down[1]
-			} else {
-				h.putUnder(t.down[0], t.down[1])
-				downi = t.down[0]
-			}
+	switch t.downl {
+	case 0:
+		downi = 0
+	case 1:
+		downi = t.down[0]
+	case 2:
+		if h.at(t.down[1]).value < h.at(t.down[0]).value {
+			h.putUnder(t.down[1], t.down[0])
+			downi = t.down[1]
 		} else {
-			h.sort2(t)
-			h.putUnder(t.down[1], t.down[2])
 			h.putUnder(t.down[0], t.down[1])
 			downi = t.down[0]
 		}
-	} else {
-		downi = 0
+	case 3:
+		h.sort3(&t.down)
+		downi = t.down[0]
+	case 4:
+		h.sort4(&t.down)
+		downi = t.down[0]
 	}
 	if at == h.top {
 		h.top = downi
-		h.at(downi).up = 0
+		down := h.at(downi)
+		down.up = 0
+		down.upi = 0
 	} else if downi == 0 {
 		up := h.at(t.up)
-		if up.down[0] == at {
-			up.down[0], up.down[1], up.down[2] =
-				up.down[1], up.down[2], 0
-		} else if up.down[1] == at {
-			up.down[1], up.down[2] = up.down[2], 0
-		} else {
-			up.down[2] = 0
+		switch t.upi {
+		case 0:
+			up.down[0] = up.down[1]
+			h.at(up.down[1]).upi--
+			fallthrough
+		case 1:
+			up.down[1] = up.down[2]
+			h.at(up.down[2]).upi--
+			fallthrough
+		case 2:
+			up.down[2] = up.down[3]
+			h.at(up.down[3]).upi--
+			fallthrough
+		case 3:
+			up.down[3] = 0
 		}
+		up.downl--
 	} else {
 		up := h.at(t.up)
-		h.at(downi).up = t.up
-		if up.down[0] == at {
-			up.down[0] = downi
-		} else if up.down[1] == at {
-			up.down[1] = downi
-		} else {
-			up.down[2] = downi
-		}
+		down := h.at(downi)
+		down.up = t.up
+		down.upi = t.upi
+		up.down[t.upi] = downi
 	}
 	*t = intArr{up: curfree}
 	return
@@ -136,90 +162,71 @@ func (h *ArrInt) putUnder(upi, downi uint32) {
 	up := h.at(upi)
 	down := h.at(downi)
 	down.up = upi
-	if up.down[2] != 0 {
-		h.sort2(up)
-		h.putUnder(up.down[1], up.down[2])
-		h.putUnder(up.down[0], up.down[1])
+	if up.downl == 4 {
+		h.sort4(&up.down)
+		up.down[3] = 0
 		up.down[2] = 0
 		up.down[1] = downi
-	} else if up.down[1] != 0 {
-		up.down[2] = downi
-	} else if up.down[0] != 0 {
-		up.down[1] = downi
+		h.at(up.down[0]).upi = 0
+		down.upi = 1
+		up.downl = 2
 	} else {
-		up.down[0] = downi
+		up.down[up.downl] = downi
+		down.upi = up.downl
+		up.downl++
 	}
 }
 
-func (h *ArrInt) putUnderI(upi, downi uint32) {
-	up := h.at(upi)
-	h.at(downi).up = upi
-	if up.down[2] != 0 {
-		t := up
-		ti := upi
-		for {
-			for t.down[2] != 0 {
-				h.sort2(t)
-				ti = t.down[1]
-				t = h.at(ti)
-			}
-			p := h.at(t.up)
-			if p.down[2] != 0 {
-				pdi := p.down[2]
-				p.down[2] = 0
-				h.at(pdi).up = ti
-				if t.down[0] == 0 {
-					t.down[0] = pdi
-				} else if t.down[1] == 0 {
-					t.down[1] = pdi
-				} else {
-					t.down[2] = pdi
-				}
-				ti = p.down[0]
-				t = h.at(ti)
-			} else {
-				pdi := p.down[1]
-				p.down[1] = 0
-				h.at(pdi).up = ti
-				if t.down[0] == 0 {
-					t.down[0] = pdi
-				} else if t.down[1] == 0 {
-					t.down[1] = pdi
-				} else {
-					t.down[2] = pdi
-				}
-				if p == up {
-					break
-				}
-				ti = t.up
-				t = p
-			}
-		}
-		up.down[1] = downi
-	} else if up.down[1] > 0 {
-		up.down[2] = downi
-	} else if up.down[0] > 0 {
-		up.down[1] = downi
-	} else {
-		up.down[0] = downi
-	}
-}
-
-func (h *ArrInt) sort2(it *intArr) {
-	v0 := h.at(it.down[0]).value
-	v1 := h.at(it.down[1]).value
-	v2 := h.at(it.down[2]).value
+func (h *ArrInt) sort4(down *[4]uint32) {
+	v0 := h.at(down[0]).value
+	v1 := h.at(down[1]).value
+	v2 := h.at(down[2]).value
+	v3 := h.at(down[3]).value
 	if v1 < v0 {
-		it.down[0], it.down[1] = it.down[1], it.down[0]
+		down[0], down[1] = down[1], down[0]
 		v1, v0 = v0, v1
 	}
 	if v2 < v1 {
-		it.down[1], it.down[2] = it.down[2], it.down[1]
+		down[1], down[2] = down[2], down[1]
 		v2, v1 = v1, v2
 		if v1 < v0 {
-			it.down[0], it.down[1] = it.down[1], it.down[0]
+			down[0], down[1] = down[1], down[0]
+			v1, v0 = v0, v1
 		}
 	}
+	if v3 < v2 {
+		down[2], down[3] = down[3], down[2]
+		v3, v2 = v2, v3
+		if v2 < v1 {
+			down[1], down[2] = down[2], down[1]
+			v2, v1 = v1, v2
+			if v1 < v0 {
+				down[0], down[1] = down[1], down[0]
+			}
+		}
+	}
+	h.putUnder(down[2], down[3])
+	h.putUnder(down[1], down[2])
+	h.putUnder(down[0], down[1])
+}
+
+func (h *ArrInt) sort3(down *[4]uint32) {
+	v0 := h.at(down[0]).value
+	v1 := h.at(down[1]).value
+	v2 := h.at(down[2]).value
+	if v1 < v0 {
+		down[0], down[1] = down[1], down[0]
+		v1, v0 = v0, v1
+	}
+	if v2 < v1 {
+		down[1], down[2] = down[2], down[1]
+		v2, v1 = v1, v2
+		if v1 < v0 {
+			down[0], down[1] = down[1], down[0]
+		}
+	}
+	h.putUnder(down[1], down[2])
+	h.putUnder(down[0], down[1])
 }
 
 func (h *ArrInt) ati(i int) *intArr {
@@ -254,10 +261,7 @@ func (h *ArrInt) getFree(tm IntValue) (t *intArr, i uint32) {
 		h.len++
 		t = h.at(i)
 	}
-	t.up = 0
-	t.down = [3]uint32{}
-	t.ref = tm
-	t.value = tm.Value()
+	*t = intArr{ref: tm, value: tm.Value()}
 	return
 }
 
